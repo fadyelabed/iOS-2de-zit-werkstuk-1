b5f605a616fed9210dacdbe02c7bdb472b1fa3f7
Fady Elabed <fady.elabed@student.ehb.be>
2019-08-08 17:49:21 +0200

    Initial Commit

---
+++ Werkstuk1Augustus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Werkstuk1Augustus
+//
+//  Created by Fady Elabed on 08/08/2019.
+//  Copyright © 2019 Fady Elabed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
